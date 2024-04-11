ba402681b2e09d0b19c40d8b43cb25d0c058fcf0
konradgveno <konradgwozdz1@gmail.com>
2024-04-11 13:05:13 +0000

    studentmachine send

---
+++ python/ppython/main.py
@@ -0,0 +1,12 @@
+print ("witaj swiecie")
+x = float( input("Podaj x "))
+y = float( input("Podaj y "))
+print(x+y)
+if x>10 :
+    print("X>10")
+elif x==10 and y==5:
+    print("X=10 and Y==5")
+else:
+    print("X<10")
+for z in range(0,10):
+    print(z)
